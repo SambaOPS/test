fe9c398c70caf5267ef481e7c5b972604e6c1443
SambaOPS <161580222+SambaOPS@users.noreply.github.com>
2025-12-22 22:37:48 +0100

    Delete scenario-15 directory

--- scenario-15/assets/setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-cp /etc/kubernetes/manifests/kube-apiserver.yaml /root/kube-apiserver.yaml.bak
-sed -i 's/:2379/:2380/g' /etc/kubernetes/manifests/kube-apiserver.yaml
-echo "Lab ready - kube-apiserver is broken, fix it!"
--- scenario-15/finish.md
+++
@@ -1,5 +0,0 @@
-# Félicitations !
-
-kube-apiserver corrigé et fonctionnel
-
-Video Solution: https://youtu.be/IL448T6r8H4

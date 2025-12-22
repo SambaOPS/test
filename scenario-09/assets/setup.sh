751a8c073b9b9c528c840064d301b2f145bd2674
SambaOPS <161580222+SambaOPS@users.noreply.github.com>
2025-12-22 22:37:10 +0100

    Delete scenario-09 directory

--- scenario-09/assets/setup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-wget -q https://github.com/Mirantis/cri-dockerd/releases/download/v0.3.20/cri-dockerd_0.3.20.3-0.debian-bullseye_amd64.deb -O ~/cri-dockerd.deb
-echo "Lab ready!"
--- scenario-09/finish.md
+++
@@ -1,5 +0,0 @@
-# Félicitations !
-
-cri-dockerd installé, service activé, paramètres sysctl configurés
-
-Video Solution: https://youtu.be/ybzo1vXiqjU

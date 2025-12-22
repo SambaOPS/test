6150c2942cb2978ff10eded08a4cf931b6647a5c
SambaOPS <161580222+SambaOPS@users.noreply.github.com>
2025-12-22 22:36:51 +0100

    Delete scenario-06 directory

--- scenario-06/assets/setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-kubectl create ns cert-manager --dry-run=client -o yaml | kubectl apply -f -
-kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.14.0/cert-manager.crds.yaml
-echo "✅ Lab ready!"

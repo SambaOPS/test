a5a997b8ec0a9eb291703cbb5ccfbca74159c25b
SambaOPS <161580222+SambaOPS@users.noreply.github.com>
2025-12-22 22:37:54 +0100

    Delete scenario-16 directory

--- scenario-16/assets/setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-kubectl create namespace relative || true
-kubectl -n relative create deployment nodeport-deployment --image=nginx --replicas=2
-echo "Lab ready!"
--- scenario-16/finish.md
+++
@@ -1,5 +0,0 @@
-# Félicitations !
-
-Deployment configuré et NodePort Service créé sur port 30080
-
-Video Solution: (lien video)
